34324ee629054f79406d6bd895d9502a2bc3f84a
ildar <UrazbakhtinIV@gmail.com>
2024-02-27 22:27:39 +0300

    incialization

--- Note.Backend/Notes.Application/obj/Debug/net8.0/Notes.Application.AssemblyInfo.cs
+++ Note.Backend/Notes.Application/obj/Debug/net8.0/Notes.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Notes.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8150e78c410a5d44ec52b4250773411ed1ee5686")]
 [assembly: System.Reflection.AssemblyProductAttribute("Notes.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Notes.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
